72d67809d2e5f10be9b03d40cd3789febd7145be
Gerardo Tzoc <gath2-96@hotmail.com>
2020-01-06 21:59:57 -0600

    added styled component package and used it instead of radium to dinamically change the style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
+//import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
+
+const StyledButton = styled.button
+`
+background-color: ${props => props.alt ? 'red' : 'green'};
+color: white;
+font: inherit;
+border: 1px solid blue;
+padding: 8px;
+cursor: pointer;
+&:hover {
+  background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+  color: black;
+}
+`;
 
 class App extends Component {
   state = {
@@ -52,33 +68,31 @@ class App extends Component {
 
   render() {
 
-      const style = {
-        backgroundColor: 'green',
-        color: 'white',
-        font: 'inherit',
-        border: '1px solid blue',
-        padding: '8px',
-        cursor: 'pointer'
-      };
+     
 
         let persons = null;
 
         if (this.state.showPersons) {
-          persons = (
-            <div >
-              {this.state.persons.map((person, index) => {
-                return <Person
-                click = {() => this.deletePersonHandler(index)}
-                 name ={person.name} 
-                age ={person.age}
-                key ={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}/>
-              })}
-            
-          </div> 
-          );
+            persons = (
 
-          style.backgroundColor = 'red';
+              <div >
+                {this.state.persons.map((person, index) => {
+                  return <Person
+                  click = {() => this.deletePersonHandler(index)}
+                  name ={person.name} 
+                  age ={person.age}
+                  key ={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
+                })}
+              
+            </div> 
+            );
+
+          // style.backgroundColor = 'red';
+          // style[':hover'] = {
+          //   backgroundColor: 'salmon',
+          //   color: 'black'
+          // };
         }
 
         let classes = [];
@@ -91,15 +105,17 @@ class App extends Component {
 
 
     return (
+      
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className = {classes.join(' ')}>This is really working!</p>
-        <button
-         style={style}
-         onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <StyledButton alt ={this.state.showPersons} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </StyledButton>
         {persons}
            
       </div>
+      
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
