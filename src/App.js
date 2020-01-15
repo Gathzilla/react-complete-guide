2667091b2d8cca5ff2670a03729e662c3cbf26d0
Gerardo Tzoc <gath2-96@hotmail.com>
2020-01-15 15:49:36 -0600

    added styled components to our app, modified the weback config dev and prod to use the css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 //import styled from 'styled-components';
 //import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
@@ -59,6 +59,7 @@ class App extends Component {
      
 
         let persons = null;
+        let btnClass = '';
 
         if (this.state.showPersons) {
             persons = (
@@ -76,6 +77,7 @@ class App extends Component {
             </div> 
             );
 
+            btnClass = classes.Red;
           // style.backgroundColor = 'red';
           // style[':hover'] = {
           //   backgroundColor: 'salmon',
@@ -83,21 +85,21 @@ class App extends Component {
           // };
         }
 
-        let classes = [];
+        let assignedClasses = [];
         if(this.state.persons.length <= 2){
-          classes.push('red');
+          assignedClasses.push(classes.red);
         }
         if(this.state.persons.length <= 1){
-          classes.push('bold');
+          assignedClasses.push(classes.bold);
         }
 
 
     return (
       
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className = {classes.join(' ')}>This is really working!</p>
-        <button className = 'button' alt ={this.state.showPersons} onClick={this.togglePersonsHandler}>
+        <p className = {assignedClasses.join(' ')}>This is really working!</p>
+        <button className = {btnClass} alt ={this.state.showPersons} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}
